4a663c94ab56334ce41e6329596aa9e5f3038cae
DrHongXu <80917018+DrHongXu@users.noreply.github.com>
2025-03-20 23:52:22 +0100

    a

--- js/main.js
+++ js/main.js
@@ -547,19 +547,27 @@ function adjustWidth(selectElement) {
 const contentListElement = document.getElementById('content-list');
 const contentList2Element = document.getElementById('content-list-2');
 const languagesElement = document.getElementById('languages');
+const languages2Element = document.getElementById('languages-2');
+const languages3Element = document.getElementById('languages-3');
 
 adjustWidth(contentListElement);
 adjustWidth(contentList2Element);
 adjustWidth(languagesElement);
+adjustWidth(languages2Element);
+adjustWidth(languages3Element);
 
 contentListElement.addEventListener('change', () => adjustWidth(contentListElement));
 contentList2Element.addEventListener('change', () => adjustWidth(contentList2Element));
 languagesElement.addEventListener('change', () => adjustWidth(languagesElement));
+languages2Element.addEventListener('change', () => adjustWidth(languages2Element));
+languages3Element.addEventListener('change', () => adjustWidth(languages3Element));
 
 window.addEventListener('resize', () => {
     adjustWidth(contentListElement);
 	 adjustWidth(contentList2Element);
     adjustWidth(languagesElement);
+	adjustWidth(languages2Element);
+	adjustWidth(languages3Element);
 });
 
 
